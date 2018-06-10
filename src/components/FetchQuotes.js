2fa3ca6f9369679a7ba598f7bb93208f1ac43ffa
bushbass <alex.nielsen@gmail.com>
2018-06-09 23:49:43 -0400

    moved api call to app.js

--- src/components/FetchQuotes.js
+++ src/components/FetchQuotes.js
@@ -2,31 +2,19 @@ import React from 'react'
 
 class FetchQuotes extends React.Component {
   
-  state = {
-    quotes: []
-  }
-
+  
   
   render() {
     const newQuoteObject = this.state.quotes[0]
     return (
       <div>
          
-        <p>{console.log(newQuoteObject)}</p>
+        <p>{console.log(props.quotes)}</p>
       </div>
     )
   }
 
-  componentDidMount() {
-    fetch("https://api.paperquotes.com/apiv1/quotes?tags=love&limit=5", {
-      headers: {
-        Authorization: "Token e2eeb1aa9f32eb07fa04595a0c457ecb6fadb772"
-      }
-    })
-      .then(response => response.json())
-      .then(myJson => this.setState({quotes:myJson.results}));
-  }
-
+  
 }
 
 export default FetchQuotes
